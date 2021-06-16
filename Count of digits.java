e697e9ecffaa429b3f164126ce802e0f55c0711d
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-06-16 20:08:59 +0530

    Create Count of digits.java

---
+++ Count of digits.java
@@ -0,0 +1,17 @@
+# Java-programs
+import java.util.*;
+public class Reverse{
+    public static void main(String[] args) {
+        System.out.println("enter number:");
+        int n=1234;
+        int count=0;
+        while(n!=0)
+
+            n=n/10;
+            count++;
+        }
+        System.out.println("No of digits="+count);
+        
+        }
+        
+}
