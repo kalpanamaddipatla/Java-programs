f740961c096ac348f98b6ca9c4cfc9c577deedf1
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-06-16 20:34:08 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+# Java-programs
+import java.util.*;
+public class Factorial {
+public static void main(string[] args){
+    System.out.println("Enter number:");
+            int n=5;
+            int f=1;
+        for(int i=1;i<=n;i++)
+        {
+          f=f*i;
+        }
+  System.out.println("Factorial of"+ n+"= "+f);
+        }
+    }
