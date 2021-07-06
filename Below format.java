23663e70228158ad63dcac0d4401aa6465418d8f
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-07-06 19:01:35 +0530

    Update Below format.java

--- Below format.java
+++ Below format.java
@@ -1,16 +1,16 @@
-# Java-programs
-public class packages {
+# Java-program
+public class pattern {
    public static void main(String[] args) {
     System.out.println("Enter number:"); 
         int n=7;
         for(int i=n-1;i>=1-n;i--)
         {
-       for(int j=n-1;j>1-n;j--)
+       for(int j=n-1;j>=1-n;j--)
        {
       if(Math.abs(i)>Math.abs(j))
-     System.out.print(Math.abs(i)+1+" ");
+     System.out.print(Math.abs(i)+1+"  ");
      else
-    System.out.print(Math.abs(j)+1+" ");
+    System.out.print(Math.abs(j)+1+"  ");
        }
        System.out.println();
         }
