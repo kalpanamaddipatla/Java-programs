b325e71f67af709014d557526e9191fce717ad64
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-07-10 09:02:29 +0530

    Create Compare to string.java

---
+++ Compare to string.java
@@ -0,0 +1,8 @@
+# Java-programs
+public class Test2 {
+    public static void main(String[] args) {
+        String words="kalpana";
+        String big="kalpi";
+      System.out.println(words.compareTo(big)); 
+    }
+}
