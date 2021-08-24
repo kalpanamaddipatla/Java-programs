7f7dad12caf59bf3a9c45d81d7d5f090c5e798bd
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-08-24 21:50:11 +0530

    Create Static method.java

---
+++ Static method.java
@@ -0,0 +1,14 @@
+# Java-programs
+package oops;
+class A1{
+    static int x=10;
+    static void show()
+    {
+    System.out.println("value="+x);
+    }
+ }
+ public class File{ 
+  public static void main(String[] args) {
+      A1.show();
+  }
+ }
